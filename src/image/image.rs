5da116dd2f292a388d5a722b76b1de9af6db2e8b
Till Schulte <till@s.chulte.de>
2023-03-27 16:44:20 +0200

    fix: error checking for avg_color function

--- src/image/image.rs
+++ src/image/image.rs
@@ -13,22 +13,26 @@ pub(crate) fn canny_algorithm(image: &GrayImage, sigma: f32) -> Detection {
 }
 
 pub(crate) fn average_color_for_rect(image: &DynamicImage, rect: &Rect<u32>) -> Rgba<u8> {
-    let colors: Vec<[u8; 4]> = (rect.min.x..rect.max.x)
+    let colors: Vec<[u8; 4]> = (rect.min.x..=rect.max.x)
         .flat_map(
             |x|
-                (rect.min.y..rect.max.y)
+                (rect.min.y..=rect.max.y)
                     .map(
                         |y|
                             image.get_pixel(x, y).0
                     ).collect::<Vec<[u8; 4]>>()
         ).collect();
 
-    let summed_red: usize = (colors.iter().map(|c| c[0] as usize).sum::<usize>()) / colors.len();
-    let summed_green: usize = colors.iter().map(|c| c[1] as usize).sum::<usize>() / colors.len();
-    let summed_blue: usize = colors.iter().map(|c| c[2] as usize).sum::<usize>() / colors.len();
-    let summed_alpha: usize = colors.iter().map(|c| c[3] as usize).sum::<usize>() / colors.len();
+    if !colors.is_empty() {
+        let summed_red: usize = (colors.iter().map(|c| c[0] as usize).sum::<usize>()) / colors.len();
+        let summed_green: usize = colors.iter().map(|c| c[1] as usize).sum::<usize>() / colors.len();
+        let summed_blue: usize = colors.iter().map(|c| c[2] as usize).sum::<usize>() / colors.len();
+        let summed_alpha: usize = colors.iter().map(|c| c[3] as usize).sum::<usize>() / colors.len();
 
-    Rgba([summed_red as u8, summed_green as u8, summed_blue as u8, summed_alpha as u8])
+        Rgba([summed_red as u8, summed_green as u8, summed_blue as u8, summed_alpha as u8])
+    } else {
+        Rgba([0, 0, 0, 0])
+    }
 }
 
 pub(crate) fn color_to_rgb_string(rgba: &Rgba<u8>) -> String {
